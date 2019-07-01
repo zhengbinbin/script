6f6a7d5a1cd7b072522139dc76b60c7f0f53a3ee
zhengbb <1475133267@qq.com>
2019-07-01 23:51:44 +0800

    端口扫描

--- port_scanner.py
+++ port_scanner.py
@@ -7,58 +7,27 @@
 import telnetlib, threading, queue
 THREADS = []
 
-def get_ip_status(ip, fileOut):
-    """取消队列，写一个类来完成"""
-    server = telnetlib.Telnet()
-    for port in range(0, 65535):
-        try:
-            server.open(ip, port)
-            print('{0} port {1} is open'.format(ip, port))
-            write_in(fileOut, port)
+def test(fileIn, fileOut):
+    fi = open(fileIn, 'r')
+    fo = open(fileOut, 'r+')
 
-        except Exception as e:
-            print('hhhh')
-            pass
-        finally:
-            server.close()
+    lines_seen = set()
 
-def write_in(fileOut, port):
-    f = open(fileOut, 'r+')
-    print('111111')
-    f.write(port)
-    f.close()
+    # for line in fi:
+    #     line = line.strip('\n')
+    #     print(line)
+    #     if line not in lines_seen:
+    #         fo.write(line + '\n')
+    #         lines_seen.add(line)
 
-def put_queue(q, fileIn):
-    f = open(fileIn)
-    values = f.readlines()
-    for value in values:
-        q.put(value)
-    f.close()
+    ts = fi.readlines()
+    print(ts)
 
-def make_thread(q, fileOut):
-    for i in range(100):
-        t = threading.Thread(target=check_open, args=(q, fileOut)) #为每个ip的每个端口创建一个线程
-        t.start()
-        THREADS.append(t)
-
-def check_open(q, fileOut):
-    try:
-        while True:
-            value = q.get_nowait()
-            A = value.split()
-            ip = A[0]
-            get_ip_status(ip, fileOut)
-    except Exception as e:
-        pass
+    fi.close()
+    fo.close()
 
 
-if __name__ == '__main__':
-    fileIn = r'E:\test.txt'
-    fileOut = r'E:\test1.txt'
-    q = queue.Queue() #创建一个队列
-    put_queue(q, fileIn)
-    make_thread(q, fileOut)
 
-
-    for thread in THREADS: #该函数是主线程，join()的作用是当数组threads里面所有的进程执行结束后再执行主线程
-        thread.join()
+fileIn = r'E:\test1.txt'
+fileOut = r'E:\test4.txt'
+test(fileIn, fileOut)
--- test.py
+++ test.py
@@ -16,43 +16,50 @@ class Scanner():
         self.fileIn = fileIn
         self.fileOut = fileOut
 
-    def sniffing(self, ip):
+    def sniffing(self, ip, f, hosts):
         """探嗅端口的方法"""
-        for port in range(22, 100):
+        for port in range(20, 23):
             try:
                 self.server.open(ip, port)
+                f.write(ip + ' ' + str(port) + '\n')
+
                 print('{0} {1} is open.'.format(ip, port))
             except Exception as err:
                 print('{0} {1} is not open.'.format(ip, port))
-                pass
             finally:
                 self.server.close()
 
 
-    def check_open(self, q):
+
+    def check_open(self, q, hosts):
         """检测队列中的信息"""
+        f = open(self.fileOut, 'a')
         try:
             while True:
                 ip = q.get_nowait()
-                sc.sniffing(ip)
+                sc.sniffing(ip, f, hosts)
         except Exception as err:
             pass
 
+        f.close()
+
     def get_host(self):
-        f = open(self.fileIn, 'r+', encoding='UTF-8')
+        #f = open(self.fileIn, 'r+', encoding='UTF-8')
+        f = open(self.fileIn, 'r+')
         hosts = f.readlines()
         for host in hosts:
             ip = host.split()
             ipList.append(ip[0])
+        f.close()
         return hosts
 
     def put_queue(self, q):
         for ip in ipList:
             q.put(ip)
 
-    def create_threads(self, q):
-        for i in range(100):
-            t = threading.Thread(target=self.check_open, args=(q,))
+    def create_threads(self, q, hosts):
+        for i in range(500):
+            t = threading.Thread(target=self.check_open, args=(q, hosts))
             t.start()
             threads.append(t)
 
@@ -62,10 +69,10 @@ if __name__ == '__main__':
 
     sc = Scanner(fileIn, fileOut)
     hosts = sc.get_host()
-
+    print(hosts)
     q = queue.Queue()
     sc.put_queue(q)
-    sc.create_threads(q)
+    sc.create_threads(q, hosts)
 
     for t in threads:
         t.join()
