7b3c65ab9c829aeb1eb1d28731ad1e1150541989
zhengbb <1475133267@qq.com>
2019-07-10 17:05:51 +0800

    mysql异地备份，只保留最近3天的备份

--- backup_mysql.py
+++ backup_mysql.py
@@ -3,12 +3,12 @@
 # @date: 2019/6/25 11:28
 # github：https://github.com/zhengbinbin
 
-import subprocess, os, tarfile, time
+import subprocess, os, tarfile, datetime, time
 
 class mysql_bak():
     """定义一个备份mysql的类"""
     def __init__(self,date, user, password, database, backup_dir, logfile,
-                          dumpfile, archive, remoteip, remotedir):
+                          dumpfile, archive, remoteip, remotedir, re_date_nuix):
         self.date = date
         self.user = user
         self.password = password
@@ -19,12 +19,11 @@ class mysql_bak():
         self.archive = archive
         self.remoteip = remoteip
         self.remotedir = remotedir
+        self.re_date_nuix = re_date_nuix
 
-    def create_file(self, dirname, filename=None):
-        self.dirname = dirname
-        self.filename = filename
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
+    def create_file(self):
+        if not os.path.exists(self.backup_dir):
+            os.makedirs(self.backup_dir)
 
         if not os.path.exists(self.logfile):
             os.mknod(self.logfile)
@@ -33,7 +32,7 @@ class mysql_bak():
         """备份数据库"""
 
         # 检测文件和文件夹是否存在
-        self.create_file(self.backup_dir, self.logfile)
+        self.create_file()
 
         # 写入备份日期到日志文件
         f = open(self.logfile, 'a')
@@ -42,10 +41,11 @@ class mysql_bak():
         os.chdir(self.backup_dir)
         dumpCmd = '/usr/local/mysql/bin/mysqldump -u' + self.user + ' -p' + self.password + ' ' + \
                   self.database + ' > ' + self.backup_dir + '/' + self.dumpfile
+        #print(dumpCmd)
         result = subprocess.call(dumpCmd, shell=True)
         if result == 0:
             with tarfile.open((self.backup_dir + '/' +  self.archive), 'w:gz') as tar:
-                tar.add(self.backup_dir + '/' + self.dumpfile, os.path.basename(self.backup_dir + '/' + self.dumpfile))
+                tar.add(self.backup_dir + '/' + self.dumpfile)
                 f.write(' 备份成功')
                 f.write('\n')
                 os.remove((self.backup_dir + '/' + self.dumpfile))
@@ -54,28 +54,39 @@ class mysql_bak():
             f.write('\n')
         f.close()
 
+        self.delete_bak()
         self.rsync_mysql()
 
     def rsync_mysql(self):
-        """使用该方法确保设置吗免密登录和目标主机建立了remotedir文件夹"""
-        self.create_file(self.remotedir)
-        cmd = 'rsync -av -e "ssh -p 52213" ' + self.backup_dir + '/' + ' xtyunweits@' + self.remoteip + ':' + self.remotedir
-        print(cmd)
-        subprocess.call(cmd, shell=True)
+        """使用该方法确保设置免密登录和目标主机建立了remotedir文件夹"""
+        cmd1 = 'rsync -av -e "ssh -p 52214" ' + self.backup_dir + '/' + ' xtyunweisolr406@' + self.remoteip + ':' + self.remotedir
+        #cmd2 = 'rsync -av -e ssh ' + self.backup_dir + '/' + ' root@' + self.remoteip + ':' + self.remotedir
+        print(cmd1)
+        subprocess.call(cmd1, shell=True)
+
+    def delete_bak(self):
+        """只保留三天的数据"""
+        for maindir, subdir, files in os.walk(self.backup_dir):
+            for file in files:
+                ret = os.path.getmtime(file)
+                if ret < self.re_date_nuix:
+                    os.remove(file)
 
 if __name__ == '__main__':
-    date = time.strftime('%Y-%m-%d', time.localtime())
+    date = (datetime.datetime.now() - datetime.timedelta(minutes=3)).strftime('%Y-%m-%d-%H-%M-%S')
     user = 'root'  # 设置mysql登陆用户名
     password = '7wg7QLyvNM8rVMT^'
     database = 'postfix'  # 要备份的数据库名
-    backup_dir = '/home/zhengbb/bak/mysql'  # 备份数据库路径
-    logfile = '/home/zhengbb/bak/log'  # 备份数据库日志文件
+    backup_dir = '/bak/mysql'  # 备份数据库路径
+    logfile = '/bak/log'  # 备份数据库日志文件
     dumpfile = date + '-mysql.sql'  # 备份后的文件名
-    archive = dumpfile + '-tar.gz'  # 压缩后的文件名
-    remoteip = '10.51.24.148'  # 异地备份机器ip（电信测试服务器）
-    remotedir = '/home/xtyunweits/bak/mysql/'  # 异地备份目录
+    archive = dumpfile + '.tar.gz'  # 压缩后的文件名
+    remoteip = '10.46.67.243'  # 异地备份机器ip（电信测试服务器）
+    remotedir = '/bak/mysql/'  # 异地备份目录
+    date_delete = datetime.datetime.now() - datetime.timedelta(days=3)
+    re_date_nuix = time.mktime(date_delete.timetuple())
 
     # 创建mysql_bak类
     mysqlBack = mysql_bak(date, user, password, database, backup_dir, logfile,
-                          dumpfile, archive, remoteip, remotedir)
+                          dumpfile, archive, remoteip, remotedir, re_date_nuix)
     mysqlBack.dump_mysql()
