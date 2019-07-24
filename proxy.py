58628d0c31ae03a2dca4831f0d53db11ba44e0ac
zhengbb <1475133267@qq.com>
2019-07-24 18:37:40 +0800

    代理脚本

---
+++ proxy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Author:郑彬彬
+# @date: 2019/7/23 16:07
+# github:https://github.com/zhengbinbin
+import subprocess
+
+class proxy():
+    def __init__(self):
+        """创建多个线程执行类里面的方法"""
+
+    def pppoe(self):
+        """拨号方法，直到拨号成功，进程睡眠"""
+
+
+    def sendInfo(self):
+        """动态IP入库"""
+
+    def checkBasic(self):
+        """检查网络和squid程序"""
+
+
+
+
+
+def main():
+    subprocess.call('find /usr/*  -name pppoe', shell=True)
+
+
+if __name__ == '__main__':
+    main()
