b1c5c2ac3075c43812bbcddff09dfb76ddadf40f
zhengbb <1475133267@qq.com>
2019-08-05 10:57:04 +0800

    代理脚本

--- protectSqiudAndProxy.py
+++ protectSqiudAndProxy.py
@@ -3,52 +3,85 @@
 # @Author:郑彬彬
 # @date: 2019/7/23 16:07
 # github:https://github.com/zhengbinbin
-import subprocess, datetime, os, sys, time
+import subprocess, datetime, os, sys, time, random
 
-def checkProxy(frequency):
-    """每1分钟检查proxy进程是否在运行"""
-    timeNow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    proxyProcessLog = '/mnt/checkProxy.log'
-    flag = 0
-    pid = os.popen('ps aux | grep proxy | grep -v "grep" | awk "{print $2}"').read()
-    while len(pid) == 0 and flag < 3:
-        flag += 1
-        subprocess.call('nohup /usr/bin/python /home/qingbo/proxy.py ' + frequency + ' &', shell=True)
-        time.sleep(1)
-        pid = os.popen('ps aux | grep proxy | grep -v "grep" | awk "{print $2}"').read()
+class ProtectSqiudAndProxy():
+    """检查proxy、squid和sendCheck进程的类"""
+    def __init__(self,port, frequency, proxyProcessLog, squidLog, sendCheckLog, timeNow):
+        self.port = port
+        self.frequency = frequency
+        self.proxyProcessLog = proxyProcessLog
+        self.squidLog = squidLog
+        self.sendCheckLog = sendCheckLog
+        self.timeNow = timeNow
 
-    if flag == 3:
-        print('连续4次重启proxy进程失败，将重启机器,本次重启将写入日志/mnt/proxyProcessLog.log')
-        with open(proxyProcessLog, 'a') as f:
-            f.write(timeNow + ': 连续4次重启proxy进程失败，将重启机器\n')
-        subprocess.call('/usr/sbin/reboot', shell=True)
+    def checkProxy(self,):
+        """每1分钟检查proxy进程是否在运行"""
+        flag = 0
+        pid = os.popen("ps aux | grep 'proxy.py' | grep -v 'grep' | awk '{print $2}'").read()
+        while len(pid) == 0 and flag < 3:
+            flag += 1
+            subprocess.call('nohup /usr/bin/python /home/qingbo/proxy.py ' + self.port + ' ' + self.frequency + ' > /dev/null 2>&1 &', shell=True)
+            time.sleep(3)
+            pid = os.popen("ps aux | grep 'proxy.py' | grep -v 'grep' | awk '{print $2}'").read()
 
-def checkSquid():
-    """每1分钟检查squid进程是否在运行"""
-    squidLog = '/mnt/checkSquid.log'
-    timeNow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    flag = 0
-    string = os.popen('systemctl status squid | grep "running"').read()
-    while len(string) == 0 and flag < 3:
-        flag += 1
-        subprocess.call('systemctl restart squid', shell=True)
+        if flag == 3:
+            print('连续4次重启proxy进程失败，将结果写入日志/mnt/proxyProcessLog.log')
+            with open(self.proxyProcessLog, 'a') as f:
+                f.write(self.timeNow + ': 连续3次重启proxy.py进程失败,将杀死所有proxy.py进程\n')
+            subprocess.call("ps -aux | grep 'proxy.py' | grep -v 'grep' | awk '{print $2}' | xargs kill", shell=True)
+
+
+    def checkSquid(self,):
+        """每1分钟检查squid进程是否在运行"""
         string = os.popen('systemctl status squid | grep "running"').read()
+        if len(string) == 0:
+            re = subprocess.call('systemctl restart squid', shell=True)
+            if re != 0:
+                with open(self.squidLog, 'a') as f:
+                    f.write(self.timeNow + ': squid程序重启失败...\n')
 
-    if flag == 3:
-        print('连续4次重启squid失败，将重启机器,本次重启将写入日志/mnt/squidLog.log')
-        with open(squidLog, 'a') as f:
-            f.write(timeNow + ': squid程序连续4次重启失败，将重启机器\n')
-        subprocess.call('/usr/sbin/reboot', shell=True)
+    def sendCheck(self,):
+        """检查sendCheck脚本是否在后台运行"""
+        flag = 0
+        pid = os.popen("ps aux | grep 'sendCheck.py' | grep -v 'grep' | awk '{print $2}'").read()
+        while len(pid) == 0 and flag < 3:
+            flag += 1
+            subprocess.call('nohup /usr/bin/python /home/qingbo/sendCheck.py' + ' ' + self.port + ' & > /dev/null 2>&1 &', shell=True)
+            time.sleep(3)
+            pid = os.popen("ps aux | grep 'sendCheck.py' | grep -v 'grep' | awk '{print $2}'").read()
+
+        if flag == 3:
+            with open(self.sendCheckLog, 'a') as f:
+                f.write(self.timeNow + ':连续3次重启sendCheck.py进程失败,将杀死所有sendCheck.py进程\n')
+            subprocess.call("ps -aux | grep 'sendCheck.py' | grep -v 'grep' | awk '{print $2}' | xargs kill", shell=True)
 
 def main():
-    if len(sys.argv) == 1:
-        print('Usage: /usr/bin/python /home/qingbo/protectSqiudAndProxy.py time(minutes) ')
-        sys.exit()
+    port = sys.argv[1]
+    if len(sys.argv) == 2:
+        # 如果没有传参数，则随机生成一个20-30之间的数
+        frequency = str(random.randint(20, 30))
+    else:
+        # 如果穿了参数就将参数给到frequency
+        frequency = sys.argv[2]
 
-    frequency = sys.argv[1]
-    checkProxy(frequency)
-    checkSquid()
+    proxyProcessLog = '/mnt/checkProxy.log'
+    squidLog = '/mnt/checkSquid.log'
+    sendCheckLog = '/mnt/sendCheck.log'
+    timeNow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    pt = ProtectSqiudAndProxy(port, frequency, proxyProcessLog, squidLog, sendCheckLog, timeNow)
+    # while True:
+    #     pt.checkProxy()
+    #     pt.checkSquid()
+    #     pt.sendCheck()
+    #     time.sleep(60)
+
+    pt.checkProxy()
+    pt.checkSquid()
+    pt.sendCheck()
+    time.sleep(60)
+
+
 
 if __name__ == '__main__':
     main()
-
--- proxy.py
+++ proxy.py
@@ -5,14 +5,12 @@
 # github:https://github.com/zhengbinbin
 import subprocess, datetime, time, os, sys
 
-class proxy():
-    def __init__(self, pppoeLog, checkUsefullog):
-        """代理类，该脚本30分钟执行一次"""
+class Proxy():
+    def __init__(self, pppoeLog, checkUsefullog, port):
+        """代理类，该脚本一直在后台执行，可设置睡眠时间，时间可设置或随机产生一个20-30分钟的数"""
         self.pppoeLog = pppoeLog
         self.checkUsefullog = checkUsefullog
-
-        self.flag = 0
-        self.flag3 = 0
+        self.port = port
 
     def getPppoe(self):
         """获得pppoe路径"""
@@ -29,83 +27,83 @@ class proxy():
 
     def checkIP(self):
         """检测IP是否通外网"""
+        flag = 0
         date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         result = os.popen('curl --connect-timeout 10 -s -w "%{http_code}" "www.baidu.com" -o /dev/null').read()
-        while result != '200' and self.flag < 3:
-            self.flag += 1
+        while result != '200' and flag < 3:
+            flag += 1
             self.pppoe()
             result = os.popen('curl --connect-timeout 10 -s -w "%{http_code}" "www.baidu.com" -o /dev/null').read()
 
-        if  self.flag == 3 or self.flag3 == 3:
+        if  flag == 3:
             with open(self.pppoeLog, 'a') as f:
-                f.write(date + '  ' + '连续 4 次拨号失败,机器将重启!!!!' + '\n')
-                #print('连续 4 次拨号失败,机器将重启!!!!')
-            subprocess.call('/usr/sbin/reboot', shell=True)
+                f.write(date + '  ' + '连续 4 次拨号失败,将杀死proxy.py进程!!!!' + '\n')
+                #print('连续 4 次拨号失败,将杀死proxy.py进程!!!!')
+            subprocess.call("ps -aux | grep 'proxy.py' | grep -v 'grep' | awk '{print $2}' | xargs kill", shell=True)
 
     def checkUseful(self):
         """执行入库检测"""
         list = []
-        flag2 = 0
         flag1 = 0
+        flag2 = 0
+        flag3 = 0
         date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         while True:
-            result = os.popen('curl -s "http://114.215.206.140:8118/syncproxy?machine_id=$HOSTNAME&port=33381&source=public_curl&purpose=公共采集"').read()
+            if self.port == '33381':
+                result = os.popen('curl -s "http://114.215.206.140:8118/syncproxy?machine_id=$HOSTNAME&port=33381&source=zhengwen_yg_curl&purpose=指数微信正文采集"').read()
+            else:
+                result = os.popen('curl -s "http://114.215.206.140:8118/syncproxy?machine_id=$HOSTNAME&port=33382&source=public_curl&purpose=公共采集"').read()
             if result == '0':
-                flag2 += 1
+                flag1 += 1
                 print('检查该ip为重复的IP，将再次进行拨号...')
                 self.pppoe()
+                self.checkIP()
             elif result == '1':
                 # 拨号成功
                 print('拨号成功，这是一个新IP')
-                flag1 += 1
+                flag2 += 1
                 break
             else:
-                print('出现一个异常,将记录异常并重新拨号')
-                self.flag3 += 1
+                print('请求API接口异常,将记录异常并重新请求')
+                flag3 += 1
                 list.append(result)
-                self.pppoe()
+                if flag3 == 3:
+                    # 连续4次请求异常将睡眠1分钟重新拨号
+                    time.sleep(60)
+                    self.pppoe()
+                    self.checkIP()
 
         with open(self.checkUsefullog, 'a') as f:
             re = os.popen('/sbin/ifconfig ppp0 |grep inet | awk "{print  $2}" |cut -d: -f2').read()
             A = re.split()  # A[1]是外网IP
-            if flag2 != 0:
-                f.write(date + '：连续 ' + str(flag2) + '次拨到重复的ip:' + '\n')
-            if self.flag3 != 0:
-                f.write('检查发现拨号异常：')
+            if flag1 != 0:
+                f.write(date + '：连续 ' + str(flag1) + '次拨到重复的ip:' + '\n')
+            if flag2 == 1:
+                f.write(date + ':有效外网ip：' + A[1] + '\n')
+                print('有效外网ip：' + A[1])
+            if flag3 != 0:
+                f.write(date + '连续' + str(flag3) + '次请求API接口异常:')
                 for string in list:
                     f.write(string + ' ')
                 f.write('\n')
-            if flag1 == 1:
-                f.write(date + ':有效外网ip：' + A[1] + '\n')
-                print('有效外网ip：' + A[1])
 
 def main():
 
-    if len(sys.argv) == 1:
-        print('Usage: /usr/bin/python /home/qingbo/proxy.py time(minutes) ')
-        sys.exit()
-
-    sleepTime = int(sys.argv[1]) * 60
+    port = sys.argv[1]
+    sleepTime = int(sys.argv[2]) * 60
 
     pppoeLog = '/mnt/pppoe.log'
     checkUsefullog = '/mnt/checkUseful.log'
 
-    p = proxy(pppoeLog, checkUsefullog)
+    p = Proxy(pppoeLog, checkUsefullog, port)
+
+    p.getPppoe()
 
     while True:
-        p.getPppoe()
         p.pppoe()
         p.checkIP()
         p.checkUseful()
         time.sleep(sleepTime)
-
-    # p.getPppoe()
-    # p.pppoe()
-    # p.checkIP()
-    # p.checkUseful()
-    # time.sleep(sleepTime)
-
-
 
 if __name__ == '__main__':
     main()
