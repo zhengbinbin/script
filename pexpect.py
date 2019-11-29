680a7e496462aa658532f5fe576e09082e6b0983
zhengbb <1475133267@qq.com>
2019-11-29 15:51:46 +0800

    交互式登陆

--- pexpect.py
+++ pexpect.py
@@ -3,16 +3,17 @@
 # @author 郑彬彬
 # @{DATE}
 # github：https://github.com/zhengbinbin
+#pythpn2上面在不不同服务器上更新代码
 
-import pexpect
+import pexpect, commands, sys
 
 PROMPT = ["# ", ">>> ", "> ", "\$ "]
 
-def connect(port, user, ip, password):
+def connect(user, ip, password):
     ssh_newkey = 'Are you sure you want to continue connecting (yes/no)?'
-    connStr =  'ssh -p %s %s@%s' % (port, user, ip)
+    connStr =  'ssh %s@%s' % (user, ip)
     child = pexpect.spawn(connStr)
-    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[p|P]assword:'])
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[p|P]assword:'], timeout=200)
     if ret == 0:
         print('[-] Error Connecting!')
         return
@@ -25,18 +26,33 @@ def connect(port, user, ip, password):
     child.expect(PROMPT)
     return child
 
-def action(child, cmd,):
-    child.sendline(cmd)
+def update(updateDir):
+    """update from svn"""
+    commands.getoutput('svn up ' + updateDir)
+    commands.getoutput('chmod -R 744 ' + updateDir)
+    commands.getoutput('chown -R nginx.nginx ' + updateDir)
+
+def action_remote(child, updateDir):
+    child.sendline('svn up ' + updateDir)
+    child.sendline('chmod -R 744 ' + updateDir)
+    child.sendline('chown -R nginx.nginx ' + updateDir)
     child.expect(PROMPT)
 
 def main():
-    port = '52195'
-    user = 'xtyunweiqbgw'
-    ip = '10.117.185.250'
-    password = 'E&OvP0suGQv4ID@m'
-    cmd = 'touch a'
-    child = connect(port, user, ip, password)
-    action(child, cmd)
+    if len(sys.argv) == 1:
+        print('usage: ./update updateDir')
+        sys.exit()
+    updateDir = sys.argv[1]
+    user = 'root'
+    ip_remote = ['172.17.94.3', '172.17.94.4']
+    password = 'Yjs2019@CLOUD!'
+    for ip in ip_remote:
+        child = connect(user, ip, password)
+        action_remote(child, updateDir)
+        print(ip + ': update success')
+
+    update(updateDir)
+    print('172.17.94.2 update success!')
 
 if __name__ == '__main__':
     main()
